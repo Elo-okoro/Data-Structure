66a7d6d4cbefbaddbaf259be244112c9b552ac89
Elo-okoro <elogheneokoro12@gmail.com>
2026-08-11 01:11:34 -0400

    Create arrayReverse.py

---
+++ arrayReverse.py
@@ -0,0 +1,5 @@
+from array import array
+Original_array =  array('i', [6, 5, 4, 3, 2, 1])
+Reversed_array = Original_array[::-1]
+print("This Is The Original Array:", Original_array)
+print("This Is It Reversed", Reversed_array)
